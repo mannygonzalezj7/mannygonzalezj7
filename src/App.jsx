646c57c5c95b035989be736794f0b3808d10d926
Manny <gonzalezjime@wisc.edu>
2025-02-28 15:49:06 -0600

    Testing browserrouter compatibility...

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./styles/App.css";
 import Header from "./components/Header";
 import Home from "./pages/Home";
