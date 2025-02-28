3b2dbb95f8829998090d4c976b3cad0b0b9e548e
Manny <gonzalezjime@wisc.edu>
2025-02-28 15:44:55 -0600

    Fixing direct routing

--- src/root.jsx
+++ src/root.jsx
@@ -2,15 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
-const path = location.pathname.substring(1);
-
-if (path && path !== "404.html") {
-  sessionStorage.setItem("redirect", "/" + path);
-  location.replace("/");
-} else {
-  ReactDOM.createRoot(document.getElementById("root")).render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  );
-}
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
